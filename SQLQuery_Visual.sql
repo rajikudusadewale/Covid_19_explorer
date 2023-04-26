4d6188e727bc34da13b8b650c334a8f84eae7c12
Kudus A. Raji <rajikudusadewale@outlook.com>
2023-04-26 14:40:30 +0100

    SQL Queries for Data Visualization in Tableau

---
+++ SQLQuery_Visual.sql
@@ -0,0 +1,40 @@
+--Queries used for Tableau Project
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+--2
+
+select continent, SUM(cast(new_deaths as int)) as TotalDeathCount from CovidDeaths
+where continent is not null
+group by continent
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+-
